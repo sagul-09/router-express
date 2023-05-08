8c9efd7d307bebdf4cc1978a8c4a9cf2681c7e96
Sagul-09 <nihal.evil.royal@gmail.com>
2023-05-08 11:16:34 +0530

    added-services

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import about from "./routes/about.js";
 import agents from "./routes/agents.js";
 import login from "./routes/login.js";
 import contact from "./routes/contact.js";
+import service from "./routes/service.js";
 
 const app = express();
 
@@ -16,6 +17,7 @@ app.use("/about", about);
 app.use("/agents", agents);
 app.use("/login", login);
 app.use("/contact", contact);
+app.use("/service", service);
 
 app.listen(1200, () => {
   console.log("Local host is working");
---
+++ routes/service.js
@@ -0,0 +1,40 @@
+import express from "express";
+const route = express.Router();
+
+route.get("/", (req, res) => {
+  res.send("Service Agents Available slect from 1-5");
+});
+
+let serviceList = [
+  {
+    id: 1,
+    service: "Web-development",
+  },
+  {
+    id: 2,
+    service: "UI-UX desiging",
+  },
+  {
+    id: 3,
+    service: "Python Development",
+  },
+  {
+    id: 4,
+    service: "AI Developmwnt",
+  },
+  {
+    id: 5,
+    service: "BAck-end Developmwnt",
+  },
+];
+route.get("/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const output = serviceList.find((service) => service.id === id);
+  console.log(id);
+  if (output) {
+    res.send("The selected service is " + '"' + output.service + '"');
+  } else {
+    res.json("Invalid agent");
+  }
+});
+export default route;
